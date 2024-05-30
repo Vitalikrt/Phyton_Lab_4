3ef5eea8699454572321deb37639c0dfe5ff87d3
Vitalikrt <125369996+Vitalikrt@users.noreply.github.com>
2024-05-30 19:50:00 +0300

    Add files via upload

---
+++ models/user.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Date
+from sqlalchemy.orm import relationship
+from database import Base
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    age = Column(Integer)
+    active = Column(Boolean, default=True)
+    registration_date = Column(Date)
+
+    records = relationship("Record", back_populates="user", cascade="all, delete-orphan")
---
+++ services/user.py
@@ -0,0 +1,38 @@
+from models.user import User
+from sqlalchemy.orm import Session
+from dto.user import User as UserDTO
+
+def create_user(data: UserDTO, db: Session):
+    user = User(**data.dict())
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
+
+def get_user(id: int, db: Session):
+    return db.query(User).filter(User.id == id).first()
+
+def update_user(id: int, data: UserDTO, db: Session):
+    user = db.query(User).filter(User.id == id).first()
+    if user:
+        if data.name is not None:
+            user.name = data.name
+        if data.email is not None:
+            user.email = data.email
+        if data.age is not None:
+            user.age = data.age
+        if data.active is not None:
+            user.active = data.active
+        if data.registration_date is not None:
+            user.registration_date = data.registration_date
+        db.commit()
+        db.refresh(user)
+    return user
+
+def delete_user(id: int, db: Session):
+    user = db.query(User).filter(User.id == id).first()
+    if user:
+        db.delete(user)
+        db.commit()
+        return {"message": "User deleted successfully"}
+    return {"message": "User not found"}
